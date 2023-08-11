7a1cf97abe47ede901d2f0ea04e0f37597ce50dd
Roma <roman.malynovsky@gmail.com>
2023-08-11 17:20:43 +0300

    done

--- lib/timer/bloc/timer_bloc.dart
+++ lib/timer/bloc/timer_bloc.dart
@@ -1,15 +1,69 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_timer/ticker.dart';
 
 part 'timer_event.dart';
+
 part 'timer_state.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
-  TimerBloc() : super(TimerInitial()) {
-    on<TimerEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final Ticker _ticker;
+  static const int _duration = 60;
+
+  StreamSubscription<int>? _tickerSubscription;
+
+  TimerBloc({required Ticker ticker})
+      : _ticker = ticker,
+        super(TimerInitial(_duration)) {
+    on<TimerStarted>(
+      (event, emit) {
+        emit(TimerRunInProgress(event.duration));
+        _tickerSubscription?.cancel();
+        _tickerSubscription = _ticker
+            .tick(ticks: event.duration)
+            .listen((duration) => add(_TimerTicked(duration: duration)));
+      },
+    );
+
+    on<_TimerTicked>(
+      (event, emit) {
+        emit(event.duration > 0
+            ? TimerRunInProgress(event.duration)
+            : TimerRunComplete());
+      },
+    );
+
+    on<TimerPaused>(
+      (event, emit) {
+        if (state is TimerRunInProgress) {
+          _tickerSubscription?.pause();
+          emit(TimerRunPause(state.duration));
+        }
+      },
+    );
+
+    on<TimerResumed>(
+      (event, emit) {
+        if (state is TimerRunPause) {
+          _tickerSubscription?.resume();
+          emit(TimerRunInProgress(state.duration));
+        }
+      },
+    );
+
+    on<TimerReset>(
+      (event, emit) {
+        _tickerSubscription?.cancel();
+        emit(const TimerInitial(_duration));
+      },
+    );
+  }
+
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
   }
 }
